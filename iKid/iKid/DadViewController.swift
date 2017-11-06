6669ec61e46d053ea0ea5069e9b930f246fe4789
sarahfeldmann <sarahf95@gmail.com>
2017-11-05 22:30:55 -0800

    add pics

--- iKid/iKid/DadViewController.swift
+++ iKid/iKid/DadViewController.swift
@@ -11,6 +11,8 @@ import UIKit
 class DadViewController: UIViewController {
     
     @IBOutlet weak var dadJokeLabel: UILabel!
+    @IBOutlet weak var forest: UIImageView!
+    @IBOutlet weak var run: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,16 +23,19 @@ class DadViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
     var flip = false
     
     @IBAction func clickFlip(_ sender: Any) {
         flip = !flip
         if(flip){
             dadJokeLabel.text = "1forrest1"
+            forest.isHidden = true
+            run.isHidden = false
         } else {
             dadJokeLabel.text =
         "What’s Forrest Gump’s password?"
+            forest.isHidden = false
+            run.isHidden = true
         }
     }
 
